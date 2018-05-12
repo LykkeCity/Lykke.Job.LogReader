fcbd564370f18a1302484913b598c0112956764a
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-05-12 22:55:47 +0300

    EnvInfo has been added to the LogDto

--- src/Lykke.Job.LogReader/PeriodicalHandlers/AzureLogHandler.cs
+++ src/Lykke.Job.LogReader/PeriodicalHandlers/AzureLogHandler.cs
@@ -285,7 +285,8 @@ namespace Lykke.Job.LogReader.PeriodicalHandlers
                         Stack = logEntity.Stack,
                         Msg = logEntity.Msg,
                         Table = table.Name,
-                        AccountName = table.Account
+                        AccountName = table.Account,
+                        Env = logEntity.Env
                     };
                     PreparingContext(dto);
 
